c6bb4a5365df5efb8ada383485c06f601f7bc499
Reese Herber <ReeseHerber@gmail.com>
2026-05-13 16:40:07 -0700

    fix: remove Create Organisation from all menus
    Single-org deployment has no use for creating additional organisations.
    Removed from both org-menu-switcher and menu-switcher.

--- apps/remix/app/components/general/org-menu-switcher.tsx
+++ apps/remix/app/components/general/org-menu-switcher.tsx
@@ -196,15 +196,6 @@ export const OrgMenuSwitcher = () => {
                   )}
                 </div>
               ))}
-
-              {isUserAdmin && (
-                <Button variant="ghost" className="w-full justify-start" asChild>
-                  <Link to="/settings/organisations?action=add-organisation">
-                    <Plus className="mr-2 h-4 w-4" />
-                    <Trans>Create Organisation</Trans>
-                  </Link>
-                </Button>
-              )}
             </div>
           </div>
 
